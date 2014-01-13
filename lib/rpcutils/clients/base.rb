549a339860ef4851a44f468444cf02205981d2f7
Anil <anil@cavvia.net>
2014-01-13 14:44:11 +0000

    autoload default client confs in base rpcclient class.

---
+++ lib/rpcutils/clients/base.rb
@@ -0,0 +1,25 @@
+require 'yaml'
+
+module RpcUtils
+  module Clients
+    class Base
+      attr_accessor :host, :port
+
+      @@defaults = {}
+
+      def initialize(opts = {})
+        @@defaults = load_defaults
+        @host = opts.fetch(:host, @@defaults['host'])
+        @port = opts.fetch(:port, @@defaults['port'])
+      end
+
+      def load_defaults
+        return @@defaults if @@defaults.size
+        yml = File.expand_path('../../../../config/clients.yml', __FILE__)
+        conf = YAML.load_file(yml)
+        name = self.class.name.downcase.split('::').last
+        conf.fetch(name, {})
+      end
+    end
+  end
+end
